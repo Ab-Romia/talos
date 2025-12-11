b3da1d076a8fbb61bee238fba1ff9967e1cccc51
Kyrollos Youssef <kyrollosyoussef02@gmail.com>
2025-12-11 03:35:26 +0200

    Added messaging model (WIP)

--- modules/model/identity.py
+++ modules/model/identity.py
@@ -1,15 +1,13 @@
 import uuid
 from datetime import datetime
-from typing import Optional, Any
 from enum import Enum as PyEnum
+from typing import Optional, Any
 
 from sqlalchemy import DateTime, UUID, Table, Column, ForeignKey, Enum
-from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.dialects.postgresql import CITEXT
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from modules.model.base import Base
-
-Base.registry.type_annotation_map[dict[str, Any]] = JSONB
 
 users_platform_roles = Table(
     "users_platform_roles", Base.metadata,
@@ -21,8 +19,8 @@ users_platform_roles = Table(
 class User(Base):
     __tablename__ = "users"
     id = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username: Mapped[str] = mapped_column(unique=True, index=True)
-    primary_email: Mapped[str] = mapped_column(unique=True, index=True)
+    username: Mapped[str] = mapped_column(CITEXT(), unique=True, index=True)
+    primary_email: Mapped[str] = mapped_column(CITEXT(), unique=True, index=True)
     email_verified: Mapped[bool] = mapped_column(default=False, index=True)
 
     name: Mapped[Optional[str]] = mapped_column()
@@ -69,6 +67,15 @@ platform_roles_permissions = Table(
     Column("platform_role_id", ForeignKey("platform_roles.id", ondelete="CASCADE"), primary_key=True),
     Column("permission_id", ForeignKey("permissions.id", ondelete="CASCADE"), primary_key=True),
 )
+
+
+class Session(Base):
+    __tablename__ = "sessions"
+    token = mapped_column(UUID(as_uuid=True), primary_key=True)
+    user_id = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime(), default=datetime.now)
+    expires_at: Mapped[datetime] = mapped_column(DateTime())
+    expired_at: Mapped[Optional[datetime]] = mapped_column()
 
 
 class PlatformRole(Base):
