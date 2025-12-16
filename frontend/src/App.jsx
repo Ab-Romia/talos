9330eba13d9acc756f90d7fe9e9ee062628777c1
aelsalmy <abdullah.elsalmy@outlook.com>
2025-12-16 22:24:53 +0200

    added react-routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import ChatPage from "./page/ChatPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <ChatPage />
+      <Routes>
+        <Route path="/" element={<h1>Landing Page to be added</h1>} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/test" element={<h1>TESTING ROUTES</h1>} />
+        <Route path="*" element={<h1>Invalid Route</h1>} />
+      </Routes>
     </>
   );
 }
